6e121775b666df21ca6238e8b1f587744808aa59
Hong Sung Jun <sjtheorange@gmail.com>
2021-07-18 14:35:50 +0000

    added pkl file that has my classifier
    overall, classifier is good, but it also makes some really dumb mistakes??

--- parse.py
+++ parse.py
@@ -3,6 +3,8 @@ import csv, requests
 from pymongo import MongoClient
 from normalize import normalize
 import numpy as np
+import joblib
+from bson.objectid import ObjectId
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
@@ -19,6 +21,14 @@ class positions(Enum):
 
 client = MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.scjiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.RiotData
+
+positionsDict = dict()
+positionsDict[0] = "JGL"
+positionsDict[1] = "TOP"
+positionsDict[2] = "MID"
+positionsDict[3] = "BOT"
+positionsDict[4] = "SUP"
+
 
 total = []
 labels = []
@@ -79,12 +89,19 @@ def readData():
 
     collection = db.newData
     cursor = collection.find({})
+    #cursor = collection.find({"_id":ObjectId("60f3d5185b92c5b595de2d72")})
+    names = []
     for document in cursor:
+        damn = []
         for participant in document["A"]:
             addData(participant)
-                
+            damn.append((participant["championName"],participant["teamPosition"]))
+            #print(participant["championName"],participant["teamPosition"])
         for participant in document["B"]:
             addData(participant)
+            damn.append((participant["championName"],participant["teamPosition"]))
+            #print(participant["championName"],participant["teamPosition"])
+        names.append(damn)
     total.append(gold_2)
     total.append(gold_4)
     total.append(gold_6)
@@ -108,14 +125,26 @@ def readData():
     print(normalized.shape)
     print(len(labels))
 
-    X_train, X_test, y_train, y_test = train_test_split(normalized,labels,random_state=1, test_size=0.2)
+    #X_train, X_test, y_train, y_test = train_test_split(normalized,labels,random_state=1, test_size=0.1)
     sc_X = StandardScaler()
-    X_trainscaled=sc_X.fit_transform(X_train)
-    X_testscaled=sc_X.transform(X_test)
+    #X_trainscaled=sc_X.fit_transform(X_train)
     
-    clf = MLPClassifier(hidden_layer_sizes=(256,128,64,32),activation="relu",random_state=1,verbose=True,max_iter=20).fit(X_trainscaled, y_train)
-    y_pred=clf.predict(X_testscaled)
-    print(clf.score(X_testscaled, y_test))
+    X_test = normalized
+    y_test = labels
+    
+    
+    X_testscaled=sc_X.fit_transform(X_test)
+    newclf = joblib.load('my_model.pkl')
+    #clf = MLPClassifier(hidden_layer_sizes=(256,128,64,32),activation="relu",random_state=1,verbose=True,max_iter=20).fit(X_trainscaled, y_train)
+    y_pred=newclf.predict(X_testscaled)
+    for i in range(len(y_pred)):
+        if y_pred[i] != y_test[i]:
+            print(names[i // 10])
+            print("predicted %s to play in lane %s(predict) instead of lane %s(true)" % (names[i//10][i%10],positionsDict[y_pred[i]],positionsDict[y_test[i]]))
+            print((i%10,y_pred[i],y_test[i]))
+        #print("prediction is %r, true label is %r" % (y_pred[i],y_test[i]))
+    print(newclf.score(X_testscaled, y_test))
+    #joblib.dump(clf, 'my_model.pkl', compress=9)
 
 
 
